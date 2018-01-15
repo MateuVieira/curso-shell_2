dea8abbe3b6a65143ab92831f9f3d52b4f748ea7
Mateus V <mateus12v@hotmail.com>
2018-01-15 17:46:30 -0200

    add script sestar apache

--- script/monitoracao_servido.sh
+++ script/monitoracao_servido.sh
@@ -2,4 +2,11 @@
 
 resposta_http=$(curl --write-out %{http_code} --silent  --output /dev/null http://localhost)
 
-echo $resposta_http 
+if [ $resposta_http -eq 200 ]
+then
+	echo "Servido ON"
+else
+	echo "Houve um problema no servidor. Tentando reiniciar"
+	systemctl restart apache2
+	echo "... Servidor reinicializado"
+fi
