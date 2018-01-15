ac7270b93807354b6ad3efdcc88e9f39da45fbbd
Mateus V <mateus12v@hotmail.com>
2018-01-15 15:22:33 -0200

    add case

--- script/filtrando-requisicao.sh
+++ script/filtrando-requisicao.sh
@@ -2,4 +2,24 @@
 
 cd ..
 
+case $1 in
+	GET)
+	cat apache.log | grep GET
+	;;
 
+	POST)
+	cat apache.log | grep POST
+	;;
+
+	PUT)
+	cat apache.log | grep PUT
+	;;
+
+	DELETE)
+	cat apache.log | grep DELETE
+	;;
+
+	*)
+	echo "O parametro passado nao é valido"
+
+esac
