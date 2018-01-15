44fdf45bd8c99841cc0b0dfd32f388434e16d8c5
Mateus V <mateus12v@hotmail.com>
2018-01-15 14:52:36 -0200

    corrigido erro if -> $1

--- script/filtrando-resultado-apache.sh
+++ script/filtrando-resultado-apache.sh
@@ -4,13 +4,10 @@ cd ..
 
 regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 
-if [[ S1 =~ $regex ]]
+if [[ $1 =~ $regex ]]
 then
-
 	cat apache.log | grep $1
-
 else
-
-	echo "Formato nao eh valido" 
+	echo "Formato nao e valido" 
 fi
 
