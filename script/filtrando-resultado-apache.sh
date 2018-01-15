3be6c89b830c673a4030dd258f4e5152480cae3d
Mateus V <mateus12v@hotmail.com>
2018-01-15 14:55:05 -0200

    add validacao do resultado

--- script/filtrando-resultado-apache.sh
+++ script/filtrando-resultado-apache.sh
@@ -7,6 +7,10 @@ regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 if [[ $1 =~ $regex ]]
 then
 	cat apache.log | grep $1
+	if [ $? -ne 0 ]
+	then
+		echo "O endereco IP não existe nos dados"
+	fi
 else
 	echo "Formato nao e valido" 
 fi
