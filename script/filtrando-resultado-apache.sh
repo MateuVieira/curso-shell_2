26bdceff42f71d4eb753d9ff537875d2ae8ddedb
Mateus V <mateus12v@hotmail.com>
2018-01-15 14:43:30 -0200

    teste regex

--- script/filtrando-resultado-apache.sh
+++ script/filtrando-resultado-apache.sh
@@ -2,4 +2,15 @@
 
 cd ..
 
-cat apache.log | grep $1
+regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
+
+if [[ S1 =~$regex ]]
+then
+
+	cat apache.log | grep $1
+
+else
+
+	echo "Formato nao eh valido" 
+fi
+
