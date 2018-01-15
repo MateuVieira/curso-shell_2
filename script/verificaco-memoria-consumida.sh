5b0b4b894be668c2913d9d08d53d79197fd80740
Mateus V <mateus12v@hotmail.com>
2018-01-15 19:33:19 -0200

    modificacao free -m

--- script/verificaco-memoria-consumida.sh
+++ script/verificaco-memoria-consumida.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-memoria_total=$(free | grep -i men | awk '{ print $2 }')
+memoria_total=$(free -m | grep -i mem | awk '{ print $2 }')
 
-memoria_consumida=$(free | grep -i men | awk '{ print $3 }')
+memoria_consumida=$(free -m | grep -i mem | awk '{ print $3 }')
+
+razo_memoria_consumida_total=$(bc <<< "scale=2;$memoria_consumida/$memoria_total *100" | awk -F. '{ print $1 }' )
+
+if [ $razo_memoria_consumida_total -gt 50 ]
+then
+	echo "acima do limite"
+else
+	echo "abaixo do limite"
+fi
